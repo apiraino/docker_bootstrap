b4ee1ddeb63da81a42e4e779495a70848e92cc6b
Marco Paolini <marco@credra.com>
2014-10-11 03:55:18 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='docker-bootstrap',
-    version='0.0.3',
+    version='0.0.4',
     description='Docker container bootstrapper',
     packages=find_packages(),
     install_requires=['jinja2', 'pyaml'],
